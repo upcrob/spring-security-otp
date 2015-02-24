aff21a053a18c45d705f897a3953cbadb61e8069
Rob Upcraft <robert.upcraft@gmail.com>
2015-02-23 20:43:26 -0600

    Updated Tokenstore documentation.

--- src/main/java/com/upcrob/springsecurity/otp/Tokenstore.java
+++ src/main/java/com/upcrob/springsecurity/otp/Tokenstore.java
@@ -5,6 +5,16 @@ package com.upcrob.springsecurity.otp;
  * a configured period of time.
  */
 public interface Tokenstore {
+	/**
+	 * Adds a username/token pair to the Tokenstore.
+	 */
 	public void putToken(String username, String token);
+
+	/**
+	 * Determines if a token is valid.  If the token is valid, this method
+	 * should return true, and invalidate the underlying token.  That is,
+	 * two successive calls to this method with the same username and
+	 * password should not return true.
+	 */
 	public boolean isTokenValid(String username, String token);
 }
