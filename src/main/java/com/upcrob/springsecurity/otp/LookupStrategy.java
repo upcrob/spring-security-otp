53529a54be767e91fc67e3b23b3ed38bac59476a
Rob Upcraft <robert.upcraft@gmail.com>
2015-03-01 19:30:26 -0600

    Updated LookupStrategy contract documentation.

--- src/main/java/com/upcrob/springsecurity/otp/LookupStrategy.java
+++ src/main/java/com/upcrob/springsecurity/otp/LookupStrategy.java
@@ -4,5 +4,12 @@ package com.upcrob.springsecurity.otp;
  * Strategy for looking up user contact information (email, phone number, etc) by username.
  */
 public interface LookupStrategy {
+	
+	/**
+	 * Lookup user contact information.  If user was not found but no error occurred then
+	 * return null.
+	 * @param username Username to lookup.
+	 * @return Contact information.
+	 */
 	public String lookup(String username);
 }
